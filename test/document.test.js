f69bec533aa8e1bdfc1dc37cae8f4fa4c786088a
mohammad0-0ahmad <kurtular.vadisi@gmail.com>
2022-03-24 17:53:31 +0100

    Add methods property in schema options to supporting auto typed document instance methods.

--- test/document.test.js
+++ test/document.test.js
@@ -11284,3 +11284,12 @@ describe('document', function() {
     });
   });
 });
+
+describe('Check if instance functions that is supplied in schema option is availabe (m0_0a)', function() {
+  it('should give an instance function back rather than undefined', function M0_0aModelJS() {
+    const testSchema = new mongoose.Schema({}, { methods: { instanceFn() { return 'Returned from DocumentInstanceFn'; } } });
+    const TestModel = mongoose.model('TestModel', testSchema);
+    const TestDocument = new TestModel({});
+    assert.equal(TestDocument.instanceFn(), 'Returned from DocumentInstanceFn');
+  });
+});
