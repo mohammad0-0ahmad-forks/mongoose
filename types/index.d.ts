152569b72db61f4c204b0cfaf0f61b010b38e7ac
mohammad0-0ahmad <kurtular.vadisi@gmail.com>
2022-04-29 00:54:23 +0200

    Refactor Model new FN

--- types/index.d.ts
+++ types/index.d.ts
@@ -259,7 +259,7 @@ declare module 'mongoose' {
 
   export const Model: Model<any>;
   export type Model<T, TQueryHelpers = {}, TMethodsAndOverrides = {}, TVirtuals = {}, TSchema = any> = NodeJS.EventEmitter & AcceptsDiscriminator & ObtainSchemaGeneric<TSchema, 'TStaticMethods'> & {
-    new<DocType = T>(doc?: DocType, fields?: any | null, options?: boolean | AnyObject): HydratedDocument<UnpackedIntersection<T, DocType>, TMethodsAndOverrides, TVirtuals>;
+    new<DocType = T>(doc?: DocType, fields?: any | null, options?: boolean | AnyObject): HydratedDocument<Omit<T, keyof DocType> & DocType, TMethodsAndOverrides, TVirtuals>;
 
     aggregate<R = any>(pipeline?: PipelineStage[], options?: mongodb.AggregateOptions, callback?: Callback<R[]>): Aggregate<Array<R>>;
     aggregate<R = any>(pipeline: PipelineStage[], cb: Function): Aggregate<Array<R>>;
